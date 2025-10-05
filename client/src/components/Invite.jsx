22b207370e00bf37798835869451531b800c4ff7
kyambuthia <kyambuthia@gmail.com>
2025-10-05 16:48:17 +0300

    feat(client): add invite component placeholder

---
+++ client/src/components/Invite.jsx
@@ -0,0 +1,11 @@
+export default function Invite() {
+  return (
+    <div className="invite">
+      <h2>Invite a Friend</h2>
+      <form>
+        <input type="text" placeholder="Enter username" />
+        <button type="submit">Send Invite</button>
+      </form>
+    </div>
+  );
+}
