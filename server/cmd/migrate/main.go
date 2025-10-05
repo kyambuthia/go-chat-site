f2dd6505af05332683d0d6e1e7def9f6814455c2
kyambuthia <kyambuthia@gmail.com>
2025-10-05 16:48:22 +0300

    feat(server): add migration runner command

---
+++ server/cmd/migrate/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/kyambuthia/go-chat-site/server/internal/migrate"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "../../chat.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := migrate.RunMigrations(db, "../migrations"); err != nil {
+		log.Fatal(err)
+	}
+}
